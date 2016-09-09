341c60c58f252e801950c4abab35ec4e42564ac2
mum-chen <542177535@qq.com>
2016-09-09 08:36:43 +0800

    the first bakup

---
+++ config.lua
@@ -0,0 +1,11 @@
+local _config = {
+	timeout = 3,
+	is_fatal = true,
+	error_code = false,
+	max_input_queue = 0,
+	max_output_queue = 0,
+	
+}
+
+
+return _config
---
+++ mqueue.lua
@@ -0,0 +1,73 @@
+
+
+
+local __default_config = require() --TODO
+
+
+
+
+
+
+
+local function request()
+
+end
+
+local function response()
+
+end
+
+local function pop()
+
+end
+
+local function push()
+
+end
+
+local function publish()
+
+end
+
+local function subscribe()
+
+end
+
+
+
+
+
+
+
+
+
+
+-- declare message-queue 
+local _m_queue = {}
+
+local function _m_queue.new(port)
+
+end
+
+local function _m_queue.setdefault()
+
+end
+
+
+local function _m_queue.get(config_table)
+
+end
+
+local function _m_queue.put(config_table)
+
+end
+
+local function _m_queue.sub(config_table)
+
+end
+
+local function _m_queue.pub(config_table)
+
+end
+
+return _m_queue 
