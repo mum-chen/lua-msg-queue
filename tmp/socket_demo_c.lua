70d4e9ed9633c44ba65f1cc801a5c984cc6d6ece
mum-chen <542177535@qq.com>
2016-09-10 08:28:23 +0800

    add lua-socket demo

---
+++ tmp/socket_demo_c.lua
@@ -0,0 +1,17 @@
+-- client.lua
+local socket = require("socket")
+host = "127.0.0.1"
+port = 8383
+
+--打开一个TCP连接
+c = assert (socket.connect (host, port))
+
+c:send ("GET \n")
+while (true) do
+ local s, status, partial = c:receive ()
+ print(s)
+ if status == "closed" then break end
+ c:send ("GET \n")
+end
+
+c:close ()
